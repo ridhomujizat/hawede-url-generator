36211f172ba7ce0c84e48155df051cca89ca613c
ridhomujizat <ridhomujizat@gmail.com>
2021-08-04 23:54:47 +0700

    generator

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
 
 function App() {
+  const [undangan, setUndangan] = React.useState("");
+  const [untuk, setUntuk] = React.useState("");
+
+  React.useEffect(() => {
+    const url = new URLSearchParams(window.location.search);
+    const myParam = url.get("undangan") || "";
+    setUndangan(myParam);
+  }, []);
+
+  const generetLink = () => {
+    const param = untuk.replaceAll(" ", "+").replaceAll("&", "%26");
+    const link = `https://undangan.hawede.id/${undangan}?for=${param}`;
+
+    return link;
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {undangan !== "" ? (
+          <>
+            <p>Undangan untuk: {undangan}</p>
+            <input
+              placeholder="input nama yang diundang"
+              value={untuk}
+              onChange={(e) => {
+                setUntuk(e.target.value);
+              }}
+            />
+            <div style={{ width: "50%" }}>
+              {untuk !== "" ? (
+                <>
+                  <p className="link">{generetLink()}</p>
+                  <button
+                    onClick={() => {
+                      navigator.clipboard.writeText(generetLink());
+                    }}
+                  >
+                    Copy Link
+                  </button>
+                </>
+              ) : (
+                <p className="link">input nama yang diundang terlebih dahulu</p>
+              )}
+            </div>
+          </>
+        ) : (
+          <p className="link">
+            Tambah code undangan di belakang url contoh url/?undangan=saya%26dia
+          </p>
+        )}
       </header>
     </div>
   );
